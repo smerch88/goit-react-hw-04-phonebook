f38fbe1af881cb370a489c8a2b73fb8aafc07d77
smerch88 <maxarmyk@gmail.com>
2022-12-29 19:51:12 +0200

    update 0 initilization

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,20 +6,14 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
+  const [contacts, setContacts] = useState(() => {
     const localContacts = JSON.parse(localStorage.getItem('contacts'));
     if (localContacts) {
-      setContacts(localContacts);
+      return localContacts;
     }
-  }, []);
+    return [];
+  });
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
